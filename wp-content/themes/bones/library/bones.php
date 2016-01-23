e13a90739b59b21f0b1e0691c931111ebfb7833c
Zach Woodward <zachw38@gmail.com>
2016-01-22 23:40:54 -0500

    smoothstate is the bane of my existence

--- wp-content/themes/bones/library/bones.php
+++ wp-content/themes/bones/library/bones.php
@@ -140,8 +140,8 @@ function bones_scripts_and_styles() {
 
 		//adding scripts file in the footer
 		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
-        wp_register_script( 'smoothState', get_stylesheet_directory_uri() . '/library/js/jquery.smoothState.min.js', array(), '', true );
-        wp_register_script( 'functions', get_stylesheet_directory_uri() . '/library/js/functions.js', array( 'smoothState', 'bones-js' ), '', true );
+        // wp_register_script( 'smoothState', get_stylesheet_directory_uri() . '/library/js/jquery.smoothState.min.js', array(), '', true );
+        // wp_register_script( 'functions', get_stylesheet_directory_uri() . '/library/js/functions.js', array( 'smoothState', 'bones-js' ), '', true );
 
 		// enqueue styles and scripts
 		wp_enqueue_script( 'bones-modernizr' );
@@ -163,8 +163,8 @@ function bones_scripts_and_styles() {
 		and your site will load faster.
 		*/
 		// wp_enqueue_script( 'jquery' );
-		// wp_enqueue_script( 'bones-js' );
-        wp_enqueue_script( 'functions' );
+		wp_enqueue_script( 'bones-js' );
+        // wp_enqueue_script( 'functions' );
 
 	}
 }
