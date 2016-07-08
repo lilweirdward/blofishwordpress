0f0b362f4bf4e484e3c43deea4892d89b363849d
Zach Woodward <zachw38@gmail.com>
2016-07-08 00:24:27 -0400

    bye bye newsflash :(

--- wp-content/themes/bones/index.php
+++ wp-content/themes/bones/index.php
@@ -24,31 +24,4 @@
 
                 </a>
 
-                <div id="newsflash">
-
-                    <div id="thebox">
-
-                        <div class="wrap">
-
-                            <!-- <img src="<?php echo get_template_directory_uri(); ?>/library/images/newsflash-banner.jpg"> -->
-                            <div class="images"></div>
-                            <div class="text">
-                                <h1>Welcome to BLoFISH!</h1>
-                                <p>
-                                    Our website is awesome, but we have something even cooler going on right now... Our <img src="<?php echo get_template_directory_uri(); ?>/library/images/kickstarter-logo.png"> for our pants and underwear.
-                                </p>
-                                <p>
-                                    Click the button below to check it out, or click the x to get back to the site.
-                                </p>
-                                <a href="https://www.kickstarter.com/projects/812532481/blofish-the-worlds-1st-gender-neutral-clothing-com?ref=nav_search" class="button" target="_blank">KICKSTARTER</a>
-                            </div>
-
-                            <i class="fa fa-times fa-2x"></i>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
 <?php get_footer(); ?>
